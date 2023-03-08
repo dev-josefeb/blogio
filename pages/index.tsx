1f82724af4f5a83b59f630f71b43df8c579ea764
dev-josefeb <febinjose@protonmail.com>
2023-03-08 17:32:43 +0100

    Create basic skelton for posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+const posts = [
+  { title: 'Test title 1', excerpt: 'First Post Test' },
+  { title: 'Test title 2', excerpt: 'Second Post Test' },
+];
+
 const Home: NextPage = () => {
   return (
     <div className='container mx-auto px-10 mb-8'>
@@ -8,6 +13,21 @@ const Home: NextPage = () => {
         <title>Blogio</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'></div>
+        </div>
+      </div>
     </div>
   );
 };
