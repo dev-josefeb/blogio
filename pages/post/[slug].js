7a461ca09f62d5d8f4bab48d089744cbc63e7ebb
dev-josefeb <febinjose@protonmail.com>
2023-03-10 16:10:10 +0100

    Fix slug dyanamic url requirement by adding getstaticPaths

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -2,20 +2,20 @@ import React from 'react'
 import {getPosts, getPostDetails} from '../../services'
 import {PostDetail, Categories, PostWidget, Author, Comments, CommentsForm} from '../../components'
 
-const PostDetails = () => {
+const PostDetails = ({post}) => {
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
             <div className='col-span-1 lg:col-span-8'>
-                <PostDetail></PostDetail>
-                <Author></Author>
-                <CommentsForm></CommentsForm>
-                <Comments></Comments>
+                <PostDetail post = {post} />
+                <Author author = {post.author}/>
+                <CommentsForm slug={post.slug} />
+                <Comments slug={post.slug} />
             </div>
             <div className='col-span-1 lg:col-span-4'>
                 <div className='relative lg:sticky top-8'>  
-                    <PostWidget></PostWidget>
-                    <Categories></Categories>
+                    <PostWidget slug ={post.slug} />
+                    <Categories categories = {post.categories.map((category) => category.slug)} />
                  </div>
 
             </div>
@@ -26,3 +26,20 @@ const PostDetails = () => {
 }
 
 export default PostDetails
+
+export async function getStaticProps({params}) {
+    const data = await getPostDetails(params.slug);
+
+    return {
+      props: { post: data },
+    };
+  }
+
+export async function getStaticPaths() {
+    const posts = await getPosts();
+
+    return {
+        paths: posts.map(({node: {slug}}) => ({params: {slug}})),
+        fallback: false,
+    }
+}
